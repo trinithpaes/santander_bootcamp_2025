c4abf2760693741a995f0468a849a54010431c0b
Trinith Paes <trinith@hotmail.com>
2025-07-03 15:19:07 -0300

    ex_07_atualizado

--- 07_operadores_aritiméticos.py
+++ 07_operadores_aritiméticos.py
@@ -1,14 +1,14 @@
-### **Adição**
-print (1 + 1)
+# Adição 
+print(1 + 1)
 
-### Subtração
-print (10 - 2)
+# Subtração
+print(10 - 2)
 
-### **Multiplicação**
-print (4 * 3)
+# Multiplicação
+print(4 * 3)
 
-### Divisão
-print (12 / 3)
+# Divisão
+print(12 / 3)
 
-### **Divisão inteira**
-print (12 // 2)
+# Divisão inteira
+print(12 // 2)
