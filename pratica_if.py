5daa66df967d5cdfe017fc7f7e9d947e8e47fb15
Trinith Paes <trinith@hotmail.com>
2025-07-09 10:02:30 -0300

    commit_atualização_1

---
+++ pratica_if.py
@@ -0,0 +1,10 @@
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+atual = saldo - saque
+
+if saldo >= saque:
+    print("Realizando saque!")
+if saldo >= saque:
+    print("Saldo insuficiente!")
+
+print("Seu saldo atual é:{} ".format(atual))
