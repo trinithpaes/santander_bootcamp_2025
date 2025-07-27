c8d0b4931a679d43ed4daaeffd30337d3c638753
Trinith Paes <trinith@hotmail.com>
2025-07-27 16:51:36 -0300

    Commit_classe_string

---
+++ 20_classe_string.py
@@ -0,0 +1,26 @@
+# Maiúscula, minúscula e título
+curso = "pYtHon"
+
+print(curso.upper())
+
+print(curso.lower())
+
+print(curso.title())
+
+
+# Eliminando espaços em branco
+curso = "   Trinith "
+
+print(curso.strip())
+
+print(curso.lstrip())
+
+print(curso.rstrip())
+
+
+# Junções e centralização
+curso = "Futebol"
+
+print(curso.center(10, "#"))
+
+print(".".join(curso))
