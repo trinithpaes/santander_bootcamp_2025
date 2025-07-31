f54dfe049b1ae30e2a35eaff4889a77ee77cf051
Trinith Paes <trinith@hotmail.com>
2025-07-31 19:04:43 -0300

    commit_fatiamento_triplas

---
+++ 23_Strings_triplas.py
@@ -0,0 +1,29 @@
+nome = "Guilherme"
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+"""
+
+
+mensagem1 = f'''
+   Olá meu nome é {nome},
+ Eu estou aprendendo Python.     
+    Essa mensagem tem diferentes recuos.
+    '''
+print (mensagem)
+print (mensagem1)
+
+print (
+    """
+    ============  MENU  ============
+       
+    1 - Depositar
+    2 - Sacar
+    3 - Sair
+       
+    ================================
+       
+         Obrigado por usar nosso sistema!!!!
+"""
+)
