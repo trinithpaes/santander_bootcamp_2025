83e327108d186806d8619a6836490ac5bd45d4c9
Trinith Paes <trinith@hotmail.com>
2025-08-30 14:26:42 -0300

    Data_e_hora

---
+++ 08_Data e Hora/38_datetime.py
@@ -0,0 +1,9 @@
+from datetime import date, datetime, time
+
+data = date(2025, 8, 28)
+print(data)
+print(data.today())
+
+data_hora = datetime(2025 ,8 ,29)
+print(data_hora)
+print(datetime.today())
